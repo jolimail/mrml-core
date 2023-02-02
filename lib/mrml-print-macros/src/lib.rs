30150b9a5946389e5119744b961bb9216664b01c
Jérémie Drouet <jeremie.drouet@gmail.com>
2023-02-02 08:48:36 +0000

    chore(print-macro): move children to separate module
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- lib/mrml-print-macros/src/lib.rs
+++ lib/mrml-print-macros/src/lib.rs
@@ -1,13 +1,12 @@
 extern crate proc_macro;
 
 mod attributes;
+mod children;
 mod element;
 
-use common_macros::{as_data_enum, as_data_struct, as_path, is_option};
 use darling::FromDeriveInput;
 use proc_macro::TokenStream;
-use quote::quote;
-use syn::{parse_macro_input, DataEnum, DataStruct, DeriveInput};
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(MrmlPrintComponent, attributes(mrml_print))]
 pub fn derive_element(input: TokenStream) -> TokenStream {
@@ -28,65 +27,5 @@ pub fn derive_attributes(input: TokenStream) -> TokenStream {
 pub fn derive_children(input: TokenStream) -> TokenStream {
     let ast: DeriveInput = parse_macro_input!(input as DeriveInput);
 
-    if let Some(data_enum) = as_data_enum(&ast) {
-        derive_children_enum(&ast, data_enum).into()
-    } else if let Some(data_struct) = as_data_struct(&ast) {
-        derive_children_struct(&ast, data_struct).into()
-    } else {
-        panic!("MrmlPrintChildren only works with enums and structs.")
-    }
-}
-
-fn derive_children_enum(ast: &DeriveInput, data_enum: &DataEnum) -> proc_macro2::TokenStream {
-    let name = &ast.ident;
-    let fields = data_enum
-        .variants
-        .iter()
-        .map(|v| {
-            let variant = &v.ident;
-            quote! {
-                #name::#variant(elt) => elt.print(pretty, level, indent_size),
-            }
-        })
-        .collect::<proc_macro2::TokenStream>();
-
-    quote! {
-        impl crate::prelude::print::Print for #name {
-            fn print(&self, pretty: bool, level: usize, indent_size: usize) -> String {
-                match self {
-                    #fields
-                }
-            }
-        }
-    }
-}
-
-fn derive_children_struct(ast: &DeriveInput, data_struct: &DataStruct) -> proc_macro2::TokenStream {
-    let name = &ast.ident;
-
-    let fields =
-        data_struct
-            .fields
-            .iter()
-            .filter_map(|f| match (&f.ident, as_path(f).map(is_option)) {
-                (Some(ident), Some(true)) => Some(quote! {
-                    if let Some(ref value) = self.#ident {
-                        res.push_str(&value.print(pretty, level, indent_size));
-                    }
-                }),
-                (Some(ident), Some(false)) => Some(quote! {
-                    res.push_str(&self.#ident.print(pretty, level, indent_size));
-                }),
-                _ => None,
-            });
-
-    quote! {
-        impl crate::prelude::print::Print for #name {
-            fn print(&self, pretty: bool, level: usize, indent_size: usize) -> String {
-                let mut res = String::new();
-                #(#fields)*
-                res
-            }
-        }
-    }
+    children::Generator::from(ast).build().into()
 }
